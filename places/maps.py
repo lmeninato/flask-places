d66b4d9eb529e99986f05bca40e411608184ae0b
lmeninato <lorenzomeninato@gmail.com>
2019-04-17 01:11:49 -0400

    Reorganizing project structure:

--- places/maps.py
+++ places/maps.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, session, g, redirect, \
-    url_for, abort, render_template, flash
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -9,14 +10,6 @@ app.config.from_envvar('APP_CONFIG_FILE', silent=True)
 MAPBOX_ACCESS_KEY = app.config['MAPBOX_ACCESS_KEY']
 
 # starting with template from http://kazuar.github.io/visualize-trip-with-flask-and-mapbox/
-@app.route('/mapbox_js')
-def mapbox_js():
-    return render_template(
-        'mapbox_js.html',
-        ACCESS_KEY=MAPBOX_ACCESS_KEY
-    )
-
-
 @app.route('/mapbox_gl')
 def mapbox_gl():
     return render_template(
